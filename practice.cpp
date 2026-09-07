ff2868c272115844a74a692e190431d7dc1414fd
Rupendrakatakam <katakamrupendra@gmail.com>
2026-09-07 21:56:39 +0530

    Expand map dimensions and search for values >= 5

--- practice.cpp
+++ practice.cpp
@@ -6,10 +6,21 @@ using namespace std;
 int main(){
     std::cout << "dc" << std::endl;
 
-    int n =2;
-    int m =2;
-    int map[n][m] = {{1,0},
-                     {0,8}};
+    int n =5;
+    int m =5;
+    int map[n][m] = {{1,0,0,0,0},
+                     {1,0,0,0,9},
+                     {1,0,5,0,0},
+                     {1,0,0,7,0},
+                     {1,0,3,0,0},
+                     };
 
     cout << "val : " << map[1][1]<< endl;
+
+    for(int i = 0; i < n; i++){
+        for (int j =0; j < m; j++){
+            if(map[i][j] >= 5) cout << "hi" << endl;
+        }
+    }
+    
 }
