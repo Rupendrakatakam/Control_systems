e93229f439700b6178b0cafa948cc54790c2f435
Rupendrakatakam <katakamrupendra@gmail.com>
2026-09-07 21:35:46 +0530

    Add practice file

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+#include "unordered_map"
+
+using namespace std;
+
+int main(){
+    std::cout << "dc" << std::endl;
+
+    int n =2;
+    int m =2;
+    int map[n][m] = {{1,0},
+                     {0,8}};
+
+    cout << "val : " << map[1][1]<< endl;
+}
